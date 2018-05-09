3ff60b4d5d33dc48142c0a54b27e9b0fe2ca068e
TheSamLloyd <samlloydj@gmail.com>
2018-05-09 00:21:31 -0400

    switched to using yarn instead of npm.... for reasons

--- models/schema.js
+++ models/schema.js
@@ -37,12 +37,13 @@ var CampaignSchema = new Schema({
     required: true
   },
   dm: {
-    type: Schema.Types.ObjectId,
+    type: String,
     required: true,
     ref: 'User'
   },
   theme: String,
-  active: {type: Boolean, default: true}
+  active: {type: Boolean, default: true},
+  system: {type: Schema.Types.ObjectId, required: true, ref: 'System'}
 })
 
 var CharacterSchema = new Schema({
@@ -50,8 +51,11 @@ var CharacterSchema = new Schema({
     type: String,
     required: true
   },
+  nickname: {
+    type: String
+  },
   user: {
-    type: Schema.Types.ObjectId,
+    type: String,
     required: true,
     ref: 'User'
   },
@@ -70,8 +74,19 @@ var CharacterSchema = new Schema({
   alive: {type: Boolean, default: true}
 })
 
+var SystemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+SystemSchema.virtual('path').get(function () {
+  return `../bot_modules/rpg/${this.name.replace(/\W+/g, '-').toLowerCase()}.js`
+})
+
 var UserObject = mongoose.model('User', UserSchema)
 var CampaignObject = mongoose.model('Campaign', CampaignSchema)
 var CharacterObject = mongoose.model('Character', CharacterSchema)
+var SystemObject = mongoose.model('System', SystemSchema)
 
-module.exports = {UserObject, CampaignObject, CharacterObject}
+module.exports = {UserObject, CampaignObject, CharacterObject, SystemObject}
