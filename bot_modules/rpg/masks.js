1edbae3514e18218645a981a5c1557e2cc334ba0
Samuel LLOYD <samuel.lloyd@amadeus.com>
2019-04-30 09:27:41 -0400

    added support for Masks system.

---
+++ bot_modules/rpg/masks.js
@@ -0,0 +1,16 @@
+var Masks = {
+  defRoll: '2d6',
+  statAlias: {
+    'dng': 'Danger',
+    'frk': 'Freak',
+    'sav': 'Savior',
+    'sup': 'Superior',
+    'mun': 'Mundane'
+  },
+  levelup: (character) => (character.exp >= 5),
+  mod: function (score) {
+    var val
+    return val
+  }
+}
+module.exports = Masks
