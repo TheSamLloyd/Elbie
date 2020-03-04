b83068fe280c2d67bc40ad785d34587d662ff771
Samuel LLOYD <samuel.lloyd@amadeus.com>
2020-03-04 11:25:23 -0500

    re-adding packages required to run audio

--- bot_modules/audio/index.ts
+++ bot_modules/audio/index.ts
@@ -1,50 +1,57 @@
 import { Module, Command } from '../module'
 import { client } from '../../elbie'
+import { User } from 'discord.js'
 
 interface QueueObject {
-    [index:string]:Queue
+    [index: string]: Queue
 }
 
-interface Queue extends Array<Track>{
-    [index:number]:Track
+interface Queue extends Array<Track> {
+    [index: number]: Track
 }
 
 class Track {
-    
+    name: string = ""
+    rqdBy?: User
+    data: any
+    constructor({ rqdBy, info }: { rqdBy: User; info: string }) {
+        //do whatever youtube interaction stuff is needed
+    }
 }
 
 class audio extends Module {
     name = 'audio'
     desc = ''
-    queue:QueueObject = {}
-    constructor(){
+    queue: QueueObject = {}
+    constructor() {
         super()
     }
-   private _play(str:string):void {
+    private _play(str: string): void {
 
-               
-    }
-    addQueue(command:Command):void{
-        if (this.queue[command.server.id]){
-            this.queue[command.server.id].push(command.argument)
-        }
-        else{
-            this.queue[command.server.id] = [command.argument]
-        }
-    }
-    stop(command:Command):void{
 
     }
-    listQueue(command:Command):void{
-        if (this.queue[command.server.id]){
-
+    addQueue(command: Command): void {
+        if (this.queue[command.server.id]) {
+            this.queue[command.server.id].push(new Track({ rqdBy: command.auth, info: command.argument }))
+        }
+        else {
+            this.queue[command.server.id] = [new Track({ rqdBy: command.auth, info: command.argument })]
         }
     }
-    play(command:Command):void{
+    stop(command: Command): void {
+
+    }
+    listQueue(command: Command): void {
+        if (this.queue[command.server.id]) {
+            this.queue[command.server.id].map((val) => { `-${val.name}` })
+        }
+    }
+    play(command: Command): void {
+        //nothing yet
 
     }
     commands = {
     }
 }
 
-export default new audio ()
+export default new audio()
