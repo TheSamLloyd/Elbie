3a97a9f3cc2ca865602722c4e825f24b1f9061bf
Samuel LLOYD <samuel.lloyd@amadeus.com>
2020-03-08 19:00:33 -0400

    80% of Fiasco is now coded and ready to go

--- bot_modules/index.ts
+++ bot_modules/index.ts
@@ -1,6 +1,7 @@
 // import all modules -- this must be done manually in TS unless we can find an elegant solution
-import rpg from "./rpg/index"
-import bot from "./bot/index"
-import audio from './audio/index'
+import rpg from "./rpg"
+import bot from "./bot"
+import audio from './audio'
+import fiasco from './fiasco'
 
-export default {rpg, bot, audio}
+export default {rpg, bot, audio, fiasco}
