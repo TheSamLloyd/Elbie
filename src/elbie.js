8eb74979d79355068f10b54e79faa7fb60afaa31
Samuel LLOYD <samuel.lloyd@amadeus.com>
2020-03-12 14:03:45 -0400

    added identification of discord hash to logs so i can bother folks

--- src/elbie.js
+++ src/elbie.js
@@ -112,7 +112,7 @@ client.on('message', function (message) {
     }
     // commands
     if (Command.command === '?' || Object.keys(commandList).includes(Command.command)) {
-      console.log(`Command: ${Command.auth.username}: ${Command.command} => ${Command.argument} `)
+      console.log(`Command: ${Command.auth.tag}: ${Command.command} => ${Command.argument} `)
       handler(Command)
     } else {
       Command.channel.send('I couldn\'t understand that command.')
