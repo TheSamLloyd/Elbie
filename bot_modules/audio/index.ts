15f8c43824434a6a3b203bb1e1c0c9612747dd4a
TheSamLloyd <samlloydj@gmail.com>
2020-04-01 22:51:26 -0400

    oh lawd audio is kind of working testing queuing now pray for me

--- bot_modules/audio/index.ts
+++ bot_modules/audio/index.ts
@@ -1,6 +1,6 @@
 import { Module, Command } from '../module'
 import ytdl from 'ytdl-core-discord'
-import { User, VoiceConnection } from 'discord.js'
+import { User, VoiceConnection, StreamDispatcher } from 'discord.js'
 
 interface QueueObject {
     [index: string]: Queue
@@ -25,6 +25,8 @@ class Track {
     id: string
     info?: any
     options: {} = {}
+    ready: boolean = false
+    stream?: any
     constructor({ rqdBy, id, options }: { rqdBy: User; id: string, options?: {} }) {
         if (options == {}) {
             options = { type: 'opus', filter: 'audioonly' }
@@ -32,9 +34,8 @@ class Track {
         this.rqdBy = rqdBy
         this.id = id
     }
-    getInfo = async () => {
+    get = async () => {
         this.info = await ytdl.getBasicInfo(this.id)
-        console.log(await this.info)
     }
 }
 
@@ -48,63 +49,60 @@ class audio extends Module {
         super()
         this.queue = {}
     }
-    private _play = async (connection: VoiceConnection, track: Track) => {
-        console.log(track.info.title, track.info.author)
-        connection.play(await ytdl(track.id), { type: 'opus' })
+    private _play = async (currQ: Queue) => {
+        let track = currQ[0]
+        currQ.connection.play(await ytdl(track.id), { type: 'opus' })
+        // let dispatcher=currQ.connection.dispatcher
+        // dispatcher.on('finish', () => {
+        //     currQ.pop()
+        //     if (currQ.length >= 1) {
+        //         let t = currQ[0]
+        //         if (t) {
+        //             this._play(currQ)
+        //             console.log(`Trying to play ${t.info.title}`)
+        //         }
+        //     }
+        // })
 
     }
-    addQueue = (command: Command): void => {
-        if (this.queue[command.server.id]) {
-            this.queue[command.server.id].push(new Track({ rqdBy: command.auth, id: command.argument }))
+    addQueue = async (command: Command): Promise<void> => {
+        let currQ = this.queue[command.server.id]
+        if (currQ) {
+            let t = new Track({ rqdBy: command.auth, id: command.argument })
+            await t.get()
+            currQ.push(t)
+            await this.listQueue(command)
+            if (currQ.length == 1) {
+                this._play(currQ)
+            }
         }
     }
-    stop = (command: Command): void => {
+    stop = async (command: Command): Promise<void> => {
         this.queue[command.server.id].connection.disconnect()
-        command.reply("Closing connection!")
+        await command.reply("Closing connection!")
     }
     listQueue = async (command: Command) => {
-        this.queue[command.server.id].forEach(async (val: Track) => {
-            console.log(val.id)
-            if (await val.info) {
-                command.reply(await val.info.title)
-            }
-            else {
-                await val.getInfo()
-                const info = await val.info
-                command.reply(await info.title)
-            }
-        })
-    }
-
-    play = (command: Command): void => {
-        this.addQueue(command)
-        const next = this.queue[command.server.id].pop()
-        if (next instanceof Track) this._play(this.queue[command.server.id].connection, next)
-        command.reply(`Now playing: ${next?.info.title}`)
-
+        await command.reply(this.queue[command.server.id].map((t: Track) => `->${t.info.title} requested by ${t.rqdBy?.username}`).join("\n"))
     }
     join = async (command: Command) => {
         if (command.member.voice.channel) {
             const connection = await command.member.voice.channel.join()
             const nQ = new Queue(connection)
             this.queue[command.server.id] = nQ
-            console.log(this.queue)
-            connection.on('finish', () => {
-                const next = this.queue[command.server.id].pop()
-                if (next instanceof Track) this._play(connection, next)
-            })
         }
         else {
-            command.reply("Sorry -- you have to join a voice channel first.")
+            await command.reply("Sorry -- you have to join a voice channel first.")
         }
     }
-    pause = (command: Command): void => {
+    pause = async (command: Command): Promise<void> => {
         let disp = this.queue[command.server.id].connection.dispatcher
-        if (disp.paused){
+        if (disp.paused) {
             disp.resume()
+            await command.reply("Playing...")
         }
-        else{
+        else {
             disp.pause(false)
+            await command.reply("Pausing...")
         }
     }
     skip = async (command: Command) => {
@@ -114,12 +112,12 @@ class audio extends Module {
     }
     commands = {
         join: { key: this.join, desc: "" },
-        play: { key: this.play, desc: "" },
-        add: {key: this.addQueue, desc:""},
+        play: { key: this.addQueue, desc: "" },
+        add: { key: this.addQueue, desc: "" },
         stop: { key: this.stop, desc: "" },
         queue: { key: this.listQueue, desc: "" },
         pause: { key: this.pause, desc: "" },
-        skip: {key:this.skip, desc:""}
+        skip: { key: this.skip, desc: "" }
     }
 }
 
